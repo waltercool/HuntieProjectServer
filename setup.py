0518b88615f1622609ba3e89fb6adb93f3049180
Pablo Cholaky <waltercool@slash.cl>
2016-08-15 17:42:20 -0300

    Updated setup.py info

--- setup.py
+++ setup.py
@@ -2,10 +2,8 @@ from setuptools import setup
 
 setup(name='huntie-server',
       version='1.0',
-      description='OpenShift App',
+      description='Server for HuntieProject',
       author='Pablo Cholaky',
       author_email='waltercool@slash.cl',
       url='http://www.python.org/sigs/distutils-sig/',
-      #install_requires=['web.py'],
-#      install_requires=['Django>=1.3'],
      )
