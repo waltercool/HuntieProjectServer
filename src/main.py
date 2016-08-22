c1a481f8b6b4216b9d6eb2d92345699e7cb46a8e
Pablo Cholaky <waltercool@slash.cl>
2016-08-22 12:22:12 -0300

    Adding more services.

--- src/main.py
+++ src/main.py
@@ -4,38 +4,55 @@ from flask import request
 app = Flask(__name__)
 
 
+# No database test of backend.
 @app.route('/test', methods=['GET'])
 def test_alive():
     return 'Server is UP'
 
 
+# Retrieve all Markets
 @app.route('/fetchMarkets', methods=['GET'])
 def fetch_markets():
-    return 'Empty'
+    result = '';
+    for key in request.args:
+        result = result.join('['+key+','+request.args[key]+'],')
+    return result
 
 
+# Retrieve an specific market
 @app.route('/fetchMarket', methods=['GET'])
 def fetch_market():
     return 'Empty'
 
 
-@app.route('/fetchShares', methods=['GET'])
-def fetch_shares():
-    return 'Empty'
-
-
-@app.route('/fetchShare', methods=['GET'])
+# Retrieve a specific stock info
+@app.route('/fetchStock', methods=['GET'])
 def fetch_share():
     return 'Empty'
 
 
-@app.route('/bidShare', methods=['POST'])
-def bid_share():
+# Buy a bid for a stock.
+@app.route('/buyShare', methods=['POST'])
+def buy_share():
     return 'Empty'
 
 
-@app.route('/retrieveShare', methods=['POST'])
-def retrieve_share():
+# Sell your bids for a specific stock.
+@app.route('/sellStock', methods=['POST'])
+def sell_share():
+    return 'Empty'
+
+
+# Retrieve all stocks bought
+@app.route('/retrieveStocks', methods=['GET'])
+def retrieve_stocks():
+    return 'Empty'
+
+
+# Handles login
+@app.route('/login', methods=['POST'])
+def login():
+    # Needs Mongo communication, should be priority.
     return 'Empty'
 
 
